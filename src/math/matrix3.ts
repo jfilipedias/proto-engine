7959e01dd6b476c0777a223a9402d4fe9ca958ba
Filipe Dias <filipediascontato@gmail.com>
2020-09-29 22:55:48 -0300

    Implement multipltication of matrix

--- src/math/matrix3.ts
+++ src/math/matrix3.ts
@@ -16,6 +16,37 @@ export class Matrix3 {
         ];
     }
 
+    // TODO
+    public inverse () {}
+
+    // TODO
+    public derterminant () {}
+
+    // TODO
+    public transpose () {}
+
+    public transform (other: Vector2): Vector2 {
+        var te = this.elements;
+
+        var vector = new Vector2(
+            te[0] * other.x + te[3] * other.y + te[6],
+            te[1] * other.y + te[4] * other.y + te[7]
+        );
+
+        var vectorW = te[2] * other.x + te[5] * other.y + te[8];
+
+        if (vectorW != 0) {
+            vector.x /= vectorW;
+            vector.y /= vectorW;
+        }
+
+        return vector;
+    }
+    
+    // TODO
+    public trs () {}
+
+
     public translate (x: number, y: number) {
         this.elements = [
             1, 0, 0,
@@ -43,21 +74,28 @@ export class Matrix3 {
         ]
     }
 
-    public transform (other: Vector2): Vector2 {
-        const te = this.elements;
+    // TODO
+    public multiply (other: Matrix3): Matrix3 {
+        var te = this.elements;
+        var oe = other.elements;
 
-        var vector = new Vector2(
-            te[0] * other.x + te[3] * other.y + te[6],
-            te[1] * other.y + te[4] * other.y + te[7]
-        );
+        var result = new Matrix3();
 
-        var vectorW = te[2] * other.x + te[5] * other.y + te[8];
+        // 1st row
+        result.elements[0] = te[0] * oe[0] + te[3] * oe[1] + te[6] + oe[2]; 
+        result.elements[3] = te[0] * oe[3] + te[3] * oe[4] + te[6] + oe[5]; 
+        result.elements[6] = te[0] * oe[6] + te[3] * oe[7] + te[6] + oe[8]; 
 
-        if (vectorW != 0) {
-            vector.x /= vectorW;
-            vector.y /= vectorW;
-        }
+        // 2st row
+        result.elements[1] = te[1] * oe[0] + te[4] * oe[1] + te[7] + oe[2]; 
+        result.elements[4] = te[1] * oe[3] + te[4] * oe[4] + te[7] + oe[5]; 
+        result.elements[7] = te[1] * oe[6] + te[4] * oe[7] + te[7] + oe[8];
 
-        return vector;
+        // 3st row
+        result.elements[2] = te[2] * oe[0] + te[5] * oe[1] + te[8] + oe[2]; 
+        result.elements[5] = te[2] * oe[3] + te[5] * oe[4] + te[8] + oe[5]; 
+        result.elements[8] = te[2] * oe[6] + te[5] * oe[7] + te[8] + oe[8];
+
+        return result;
     }
 }

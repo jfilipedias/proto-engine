03085429d0bf7ce61523ea03296a4cc409211415
Filipe Dias <filipediascontato@gmail.com>
2020-09-29 21:24:05 -0300

    Implement translate, scale and rotate operations on Matrix3 class

--- src/math/matrix3.ts
+++ src/math/matrix3.ts
@@ -1,3 +1,54 @@
+import { Vector2 } from "./vector2";
+
 export class Matrix3 {
-    
+    elements: number[] = [];
+
+    public constructor () {
+        this.identity();
+    }
+
+    // Collum-Major notation
+    public identity () {
+        this.elements = [
+            1, 0, 0,        // 1st collum
+            0, 1, 0,        // 2nd collum
+            0, 0, 1,        // 3rd collum
+        ];
+    }
+
+    public translate (x: number, y: number) {
+        this.elements = [
+            1, 0, 0,
+            0, 1, 0,
+            x, y, 1,
+        ];
+    }
+
+    public scale (x: number, y: number) {
+        this.elements = [
+            x, 0, 0,
+            0, y, 0,
+            0, 0, 1,
+        ];
+    }
+
+    public rotate (theta: number) {
+        var cos = Math.cos(theta);
+        var sin = Math.sin(theta);
+
+        this.elements = [
+             cos, sin, 0,
+            -sin, cos, 0,
+               0,   0, 1,
+        ]
+    }
+
+    /* public transform (other: Vector2): Vector2 {
+        var vector = new Vector2(
+            this.elements[0] * other.x + this.elements[3] * other.y + this.elements [6],
+            this.elements[1] * other.y + this.elements[4] * other.y + 
+        );
+
+        return other;
+    } */
 }

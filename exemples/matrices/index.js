fa2aeb47e21af0863e7a4160870736c59b30d2f9
Filipe Dias <filipediascontato@gmail.com>
2020-10-03 00:05:49 -0300

    Implement functions to draw squares

--- exemples/matrices/index.js
+++ exemples/matrices/index.js
@@ -1,17 +1,45 @@
-var vectorsList = [];
+var squareVectors = [];
 
 function setup () {
 	createCanvas(600, 600);
 
+	// World Origin
 	this.origin = new Vector2(width/2, height/2);
+	
+	squareVectors = createSquare();
 }
 
 function draw () {
-	// Grid Y axis
-  stroke( 50, 50, 50 );
-	line(this.origin.x, 0, this.origin.x, height);
+	drawGrid();
 	
+	drawSquare(squareVectors);
+}
+
+function drawGrid () {Bottom
+	// Grid Y axis
+	stroke( 50, 50, 50 );
+	line(this.origin.x, 0, this.origin.x, height);
+
 	// Grid X axis
-  stroke( 50, 50, 50 );
-  line(0, this.origin.y, width, this.origin.y);  
+	stroke( 50, 50, 50 );
+	line(0, this.origin.y, width, this.origin.y);  
+}
+
+function createSquare (positionX, positionY, size) {
+	var vectors = [];
+	
+	// Right Top Corner
+	vectors.push(new Vector2(positionX + size/2, positionY - size/2));
+	// Left Top Corner
+	vectors.push(new Vector2(positionX - size/2, positionY - size/2));
+	// Left Bottom Corner
+	vectors.push(new Vector2(positionX - size/2, positionY + size/2));
+	// Right Bottom Corner
+	vectors.push(new Vector2(positionX + size/2, positionY + size/2));
+
+	return vectors;
+}
+
+function drawSquare (vectors) {
+
 }

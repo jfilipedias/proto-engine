aa6c772b173ebcdae0bca063605d781f6312952d
Filipe Dias <filipediascontato@gmail.com>
2020-10-13 11:33:58 -0300

    Add a method to check collision with a vector

--- src/collision/aabb.js
+++ src/collision/aabb.js
@@ -1,17 +1,41 @@
 class AABB {
     constructor (vectors) {
-        this.min = new Vector2();
-        this.max = new Vector2();
+        this.min = new Vector2( Infinity,  Infinity);
+        this.max = new Vector2(-Infinity, -Infinity);
 
-        if (vectors.length <= 0)
+        if (vectors === undefined || vectors === null || vectors.length <= 0)
             return;
-        
+            
         for (var i = 0; i < vectors.length; i++) {
-            this.min.x = Math.min(this.min.x, vectors[i].x);
-            this.min.y = Math.max(this.min.y, vectors[i].y);    // Screen Y coordinate is inverted
+            var vector = vectors[i];
 
-            this.max.x = Math.max(this.max.x, vectors[i].x);
-            this.max.y = Math.min(this.max.y, vectors[i].y);    // Screen Y coordinate is inverted
+            this.min.x = Math.min(this.min.x, vector.x);
+            this.min.y = Math.min(this.min.y, vector.y);
+
+            this.max.x = Math.max(this.max.x, vector.x);
+            this.max.y = Math.max(this.max.y, vector.y);
         }
+    }
+
+    IsEmpty () {
+        return this.min.x ==  Infinity && this.min.y ==  Infinity 
+            && this.max.x == -Infinity && this.max.y == -Infinity
+    }
+
+    collides (other) {
+        var apart = 
+            this.min.x > other.max.x ||
+            this.min.y > other.max.y ||
+            this.max.x < other.min.x ||
+            this.max.y < other.min.y;
+        
+        return !apart;
+    }
+
+    contains (vector) {
+        return vector.x >= this.min.x
+            && vector.x <= this.max.x
+            && vector.y >= this.min.y
+            && vector.y <= this.max.y; 
     }
 }

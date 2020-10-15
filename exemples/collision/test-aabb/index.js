c2ea5f770816ca60574b813343ff370277eabf0e
Filipe Dias <filipediascontato@gmail.com>
2020-10-15 17:30:26 -0300

    Draw a point cloud in aabb exemple

--- exemples/collision/test-aabb/index.js
+++ exemples/collision/test-aabb/index.js
@@ -1,35 +1,55 @@
-var square;
 var origin;
+var cloudPointsA;
+var cloudPointsB;
 
 function setup () {
 	createCanvas(600, 600);
 
 	origin = new Vector2(width/2, height/2);
-	squareA = new Square(100, 100, 80);
-    squareA.pivot = squareA.pivot.add(origin);
-    
-    squareB = new Square(220, 180, 150);
-    squareB.pivot = squareB.pivot.add(origin);
+	
+	cloudPointsA = createCloud(-35, 100, 50, 90, 100);
+    cloudPointsB = createCloud(0, 20, 50, 90, 100);
+    //cloudPointsB = createCloud(100, 20, 50, 90, 100);	 // This dont collide
 }
 
 function draw () {
 	background(255);
-    drawGrid();
+	drawGrid();
+	
+	stroke(30);
+    drawCloud(cloudPointsA);
+
+    stroke(232, 23, 110);
+    drawCloud(cloudPointsB);
     
-    var aabb1 = new AABB(squareA.points);
-    var aabb2 = new AABB(squareB.points);
+    var aabbA = new AABB(cloudPointsA);
+    var aabbB = new AABB(cloudPointsB);
     
     stroke(35, 110, 230);
-    if (aabb1.collides(aabb2))
+    if (aabbA.collides(aabbB))
         stroke(242, 55, 41);
     
-    drawSquare(squareA.points);
+    drawSquare(aabbA);
 
     stroke(35, 110, 230);
-    if (aabb1.collides(aabb2))
+    if (aabbA.collides(aabbB))
         stroke(242, 55, 41);
 
-	drawSquare(squareB.points);
+	drawSquare(aabbB);
+}
+
+function createCloud (x, y, width, height, n) {
+    var points = [];
+
+    for (var i = 0; i < n; i++)
+        points.push(new Vector2( (x + Math.random() * width - i * 0.2), (y + Math.random() * height + i * 0.2) ));
+
+    return points;
+}
+
+function drawCloud (points) {
+    for (var i = 0; i < points.length; i++)
+        circle(origin.x + points[i].x, origin.y - points[i].y, 2);
 }
 
 function drawGrid () {
@@ -52,16 +72,15 @@ function drawGrid () {
 	text("X", width - 10, origin.y + 20);
 }
 
-function drawSquare (points) {
-    var aabb = new AABB(points);
+function drawSquare (aabb) {
 
     if (aabb.contains(getMousePosition()))
 	    stroke(30, 30, 30);
 
-	line(origin.x + points[0].x, origin.y - points[0].y, origin.x + points[1].x, origin.y - points[1].y);
-	line(origin.x + points[1].x, origin.y - points[1].y, origin.x + points[2].x, origin.y - points[2].y);
-	line(origin.x + points[2].x, origin.y - points[2].y, origin.x + points[3].x, origin.y - points[3].y);
-	line(origin.x + points[3].x, origin.y - points[3].y, origin.x + points[0].x, origin.y - points[0].y);
+	line(origin.x + aabb.max.x, origin.y - aabb.max.y, origin.x + aabb.min.x, origin.y - aabb.max.y);
+	line(origin.x + aabb.min.x, origin.y - aabb.max.y, origin.x + aabb.min.x, origin.y - aabb.min.y);
+	line(origin.x + aabb.min.x, origin.y - aabb.min.y, origin.x + aabb.max.x, origin.y - aabb.min.y);
+	line(origin.x + aabb.max.x, origin.y - aabb.min.y, origin.x + aabb.max.x, origin.y - aabb.max.y);
 }
 
 function getMousePosition () {

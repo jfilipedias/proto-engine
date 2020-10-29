14a6a66668d0dbf4c6689c6876e8f4e85ff01c8a
Filipe Dias <filipediascontato@gmail.com>
2020-10-28 22:49:22 -0300

    Move SAT algorithm to OBB class

--- src/math/obb.js
+++ src/math/obb.js
@@ -51,6 +51,51 @@ class OBB {
         }
     }
 
+    collides(other) {
+        var points = this.getPoints();
+
+        var edges = [];       
+        edges.push(new Vector2(points[1].x - points[0].x, points[1].y - points[0].y));
+        edges.push(new Vector2(points[2].x - points[1].x, points[2].y - points[1].y));
+        edges.push(new Vector2(points[3].x - points[2].x, points[3].y - points[2].y));
+        edges.push(new Vector2(points[0].x - points[3].x, points[0].y - points[3].y));
+
+
+        var otherPoints = other.getPoints();
+
+        var otherEdges = [];
+        otherEdges.push(new Vector2(otherPoints[1].x - otherPoints[0].x, otherPoints[1].y - otherPoints[0].y));
+        otherEdges.push(new Vector2(otherPoints[2].x - otherPoints[1].x, otherPoints[2].y - otherPoints[1].y));
+        otherEdges.push(new Vector2(otherPoints[3].x - otherPoints[2].x, otherPoints[3].y - otherPoints[2].y));
+        otherEdges.push(new Vector2(otherPoints[0].x - otherPoints[3].x, otherPoints[0].y - otherPoints[3].y));
+
+        var separatingAxis = this.getSeparatingAxis(edges, otherEdges);
+
+        var apart = Boolean(separatingAxis !== null);
+        return !apart;
+    }
+
+    collidesAABB(aabb) {
+        var aabbEdges = [];
+        aabbEdges.push(new Vector2(aabb.max.x - aabb.min.x, aabb.min.y));
+        aabbEdges.push(new Vector2(aabb.max.x, aabb.min.y - aabb.max.y));
+        aabbEdges.push(new Vector2(aabb.min.x - aabb.max.x, aabb.max.y));
+        aabbEdges.push(new Vector2(aabb.min.x, aabb.max.y - aabb.min.y));
+
+        var obbPoints = this.getPoints();
+
+        var obbEdges = [];
+        obbEdges.push(new Vector2(obbPoints[1].x - obbPoints[0].x, obbPoints[1].y - obbPoints[0].y));
+        obbEdges.push(new Vector2(obbPoints[2].x - obbPoints[1].x, obbPoints[2].y - obbPoints[1].y));
+        obbEdges.push(new Vector2(obbPoints[3].x - obbPoints[2].x, obbPoints[3].y - obbPoints[2].y));
+        obbEdges.push(new Vector2(obbPoints[0].x - obbPoints[3].x, obbPoints[0].y - obbPoints[3].y));
+
+        var separatingAxis = this.getSeparatingAxis(aabbEdges, obbEdges);
+
+        var apart = Boolean(separatingAxis !== null);
+        return !apart;
+    }
+
     contains(point) {
         var u = new Vector2(1, 0);
         u = this.matrix.transform(u);
@@ -100,5 +145,38 @@ class OBB {
         points.push(obb.center.add((u.multiplyScalar(obb.extent.x)).add(v.multiplyScalar(obb.extent.y))));
 
         return points;
+    }
+
+    getSeparatingAxis(edgesA, edgesB) {
+        var edges = edgesA.concat(edgesB);
+
+        for (var i = 0; i < edges.length; i++) {
+            var separatingAxis = new Vector2(edges.y, -edges.x);
+            
+            var minA = +Infinity;
+            var maxA = -Infinity;
+
+            for (var j = 0; j < edgesA.length; j++) {
+                var projection = edgesA.dot(separatingAxis);
+
+                minA = min(minA, projection);
+                maxA = max(maxA, projection);
+            }
+
+            var minB = +Infinity;
+            var maxB = -Infinity;
+
+            for (var j = 0; j < edgesB.length; j++) {
+                var projection = edgesB.dot(separatingAxis);
+
+                minB = min(minB, projection);
+                maxB = max(maxB, projection);
+            }
+
+            if (maxA < minB || maxB < minA)
+                return separatingAxis;
+        }
+
+        return null;
     }
 }
